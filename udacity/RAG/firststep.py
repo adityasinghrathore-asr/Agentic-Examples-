e06bec08435f78e4c1500dfef6a31a04d5874c0c
Aditya Rathore <aditya.singh.rathore@accenture.com>
2026-03-24 20:04:42 -0700

    all commit secret removed

--- udacity/RAG/firststep.py
+++ udacity/RAG/firststep.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 from pprint import pprint
 
-os.environ['OPENAI_API_KEY'] = "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"
+os.environ['OPENAI_API_KEY'] = ""
 
 #print(os.environ)
 
